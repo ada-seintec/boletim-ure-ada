576abc1c62288b1978c446e1cebdd92c7c94f793
Leonardo Menezes Capetta <leonardo.capetta@educacao.sp.gov.br>
2025-11-24 13:52:39 -0300

    boletim 45 de 2025

---
+++ edicoes/2025/45/js/data.js
@@ -0,0 +1,39 @@
+// Caminho: ./js/data.js
+
+const BOLETIM_MARKDOWN_CONTENT = `edicao: 45
+data: 24/11/2025
+
+## ure-adamantina: UNIDADE REGIONAL DE ENSINO
+tipo: lista
+nomeArquivo: Edital_Tutor.pdf
+titulo: Processo Seletivo Interno para atuação no Projeto Professor Tutor Anos Finais – Edital de Abertura de Inscrições
+resumo: A Comissão Especial de Contratação por Tempo Determinado da Unidade Regional de Ensino – Região **Adamantina** torna pública a abertura de inscrições e a realização do Processo Seletivo para docentes, em nível regional, visando à atuação no Projeto Professor Tutor nos Anos Finais do Ensino Fundamental para 2026, instituído pela Resolução SEDUC Nº 140 de 18 de novembro de 2025, na rede estadual de ensino. A seleção ocorrerá por meio de inscrição via Banco de Talentos e entrevista, conforme as condições estabelecidas neste edital. 
+
+
+## sepes: SERVIÇO DE PESSOAS
+tipo: lista
+nomeArquivo: "Recadastramento IAMSPE.pdf"
+titulo: Recadastramento IAMSPE
+resumo: O recadastramento do **IAMSPE – Instituto de Assistência Médica ao Servidor Público Estadual de São Paulo** já está disponível e o prazo está chegando ao fim, que é **até 30/11/2025**.
+
+
+## sggd: SECRETARIA DE GESTÃO E GOVERNO DIGITAL
+tipo: lista
+nomeArquivo: "Recadastramento Digital 2025.pdf"
+titulo: Recadastramento Digital 2025
+resumo: Informamos que o **Recadastramento Digital 2025**, **atividade obrigatória para todos os servidores ativos, civis e militares, das Secretarias, Autarquias e Fundações do Executivo** do Estado de São Paulo, **já está disponível**.
+
+
+## suped: SUBSECRETARIA PEDAGÓGICA
+tipo: lista
+nomeArquivo: "suped 1.pdf"
+titulo: Live com o escritor e roteirista Raphael Montes; Convite para Reunião sobre o programa Florestas nas Escolas; informações sobre o Projeto Mediação & Linguagem, Guia Digital do PNLD Literário - Anos Finais do Ensino Fundamental, Dia da Consciência Negra; atualizações e orientações para os estudantes e unidades envolvidas no Programa Prontos e informações sobre o processo seletivo do referido Programa.
+resumo:  COPLAE - Informa sobre a Live com o escritor e roteirista Raphael Montes; CORRIC – Convida para reunião sobre o programa Florestas nas Escolas; DIMOD – Agradece a participação das URE no Projeto Mediação & Linguagem e informa sobre a Mostra das Produções Finalistas; orienta sobre o acesso ao Guia Digital do PNLD Literário - Anos Finais do Ensino Fundamental e sobre a escolha das obras; informa sobe o Dia 20 de Novembro - Dia da Consciência Negra, atualiza informações e orienta os estudantes e unidades envolvidas no Programa Prontos pro Mundo.
+
+
+## efape: ESCOLA DE FORMAÇÃO E APERFEIÇOAMENTO DOS PROFISSIONAIS DA EDUCAÇÃO
+tipo: lista
+nomeArquivo: "efape 1.pdf"
+titulo: Escola de Gestão 1ª Edição - 2025 - Cronograma da Semana de 24 a 28/11; Multiplica SP 25.2 | Encerramento do período da Socialização e Portfólio; Multiplica SP 2025.2 | Próximas semanas de Formações
+resumo: O Boletim nº 46 da EFAPE informa sobre o cronograma da semana 24 a 28/11 do curso Escola de Gestão; O Multiplica SP 25.2 traz um panorama sobre o envio das socializações e portfólio, além de incluir informações sobre as próximas semanas de formações.
+`;
