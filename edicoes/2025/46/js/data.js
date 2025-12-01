1af92b71093499603368f1885cf0b6048e62e542
ada-seintec <ada.seintec@educacao.sp.gov.br>
2025-12-01 11:59:57 -0300

    Add files via upload
    Adicionando o data.js da versão 46

--- edicoes/2025/46/js/data.js
+++ edicoes/2025/46/js/data.js
@@ -1,95 +1,85 @@
 // Caminho: ./js/data.js
 
-const BOLETIM_MARKDOWN_CONTENT = `edicao: XX
-data: DD/MM/AAAA
+const BOLETIM_MARKDOWN_CONTENT = `edicao: 46
+data: 01/12/2025
 
 ## ure-adamantina: UNIDADE REGIONAL DE ENSINO
 tipo: card
-nomeImagem: 
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeImagem: "conviva.jpg"
+nomeArquivo: "Relatório Encontro Conviva 25-11-2025.pdf"
+titulo: Relatório Encontro Conviva 25/11/2025
+resumo: A formação “Elos e Saberes: Tecendo Conhecimentos, Fortalecendo Vínculos na Educação” realizada no dia 25 de novembro de 2025, no Polo de Capacitação da Unidade Regional de Ensino de Adamantina.
 
+tipo: card
+nomeImagem: "avaliacao_institucional.png"
+nomeArquivo: "Avaliação Institucional.pdf"
+titulo: Avaliação Institucional
+resumo: Prezados Diretores, convidamos sua equipe a participar da Avaliação Institucional da Unidade Regional de Ensino de Adamantina.
 
 ## escolas-estaduais: ESCOLAS ESTADUAIS
 tipo: card
-nomeImagem: "joao bernardi.png"
-nomeArquivo: "Joao Bernardi.pdf"
-titulo: Documentário destaca transformações da Escola João Bernardi
-resumo: O documentário foi uma ação da Sala de Leitura, coordenada pela professora Inês Furlanetti, com a participação especial e o protagonismo dos estudantes da 2ª série do Ensino Médio. A produção retratou o antes e depois da reforma da Escola João Bernardi, evidenciando as mudanças que marcaram sua história.
+nomeImagem: "projeto_de_vida.jpg"
+nomeArquivo: "PROJETO DE VIDA.pdf"
+titulo: E E Orlando Projeto de vida
+resumo: Projeto de Vida nasceu da necessidade de tornar as aulas mais atrativas e significativas
 
 tipo: card
-nomeImagem: 
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeImagem: "palestra.jpg"
+nomeArquivo: "PALESTRA TÉCNICO ADM JOSÉ FIRPO.pdf"
+titulo: Palestra sobre Preparação para Processo Seletivo mobiliza estudantes do Técnico em Administração da E.E. José Firpo
+resumo: No dia 25 de novembro, os alunos do Ensino Técnico em Administração da E.E. José Firpoparticiparam de uma palestra formativa sobre o tema “Preparação para o Processo Seletivo e Busca de Oportunidades de Trabalho”.
 
+tipo: card
+nomeImagem: "joao bernardi.png"
+nomeArquivo: "Joao Bernardi.pdf"
+titulo: Documentário destaca as trasnformações da escola João Bernardi
+resumo: O documentário foi uma ação da Sala de Leitura, coordenada pela professora Inês Furlanetti, com a participação especial e o protagonismo dos estudantes da 2ª série do Ensino Médio.
 
-## seafin: ​​SEAFIN – SERVIÇO DE ADMINISTRAÇÃO E FINANÇAS
-tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
-
-
-## segre: SEGRE – SERVIÇO DE GESTÃO DA REDE ESCOLAR
-tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
-
-
-## seintec: SERVIÇO DE INFORMAÇÕES EDUCACIONAIS E TECNOLOGIA
-tipo: lista
-nomeArquivo: 
-titulo: 
+tipo: card
+nomeImagem: "carrara.jpg"
+nomeArquivo: "Carrara.pdf"
+titulo: Cerimônia de Premiação da ONEE 2025 celebra excelência estudantil na Escola Carrara. 
 resumo:
 
+tipo: card
+nomeImagem: "fleurides.png"
+nomeArquivo: "Maratona Tech.pdf"
+titulo: Aluno de Adamantina Conquista Medalha de Prata na Maratona Tech
+resumo: O aluno Rafael Antônio Gualti Guerreiro, do 6º ano B da Escola Estadual Fleurides Cavallini Menechino, em Adamantina, São Paulo, conquistou a medalha de prata no Nível 1 da Maratona Tech 2025.
 
-## seom: SERVIÇO DE OBRAS E MANUTENÇÃO ESCOLAR
-tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
-
+tipo: card
+nomeImagem: "waldomiro.png"
+nomeArquivo: "waldomiro.pdf"
+titulo: Dia da Consiência Negra
+resumo:A Escola Estadual Prefeito Waldomiro Sampaio de Souza no dia 19/11/2025 o Dia da Consciência Negra e colocou em evidência “As Mulheres que se destacaram na Educação Brasileira”.
 
 ## sepes: SERVIÇO DE PESSOAS
 tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeArquivo: "RESOLUÇÃO SGGD Nº 54.pdf"
+titulo: RESOLUÇÃO SGGD Nº 54, DE 28-11-2025
+resumo: Prorroga o prazo previsto no artigo 1º da Resolução SGGD nº 45, de 24 de outubro de 2025.
 
-
-## seduc: SECRETARIA DA EDUCAÇÃO DO ESTADO DE SÃO PAULO
 tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
-
-
-## sggd: SECRETARIA DE GESTÃO E GOVERNO DIGITAL
-tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
-
+nomeArquivo: "https://www.iamspe.sp.gov.br/prorrogado-o-recadastramento-iamspe/"
+titulo: PRORROGADO: Prazo da realizar o Recadastramento Iamspe é estendido até 31 de dezembro de 2025
+resumo: O prazo para a realizar o Recadastramento do Instituto de Assistência Médica ao Servidor Público Estadual (Iamspe) foi estendido até o dia 31 de dezembro de 2025. A atualização deve ser feita pelos titulares do convenio, que devem também inserir os dados de seus dependentes, no aplicativo “Iamspe Digital”, no espaço destinado ao serviço. Caso surjam dúvidas sobre o preenchimento dos formulários, estão disponíveis matérias com as principais dúvidas, além de um vídeo tutorial, ambos no site do Iamspe.
 
 ## suart: SUBSECRETARIA DE ARTICULAÇÃO DA REDE DE ENSINO
 tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeArquivo: "suart.pdf"
+titulo: Avaliação de Fluência Leitora – Saída 2025
+resumo: COMUNICADO EXTERNO CONJUNTO - Subsecretaria de Articulação da Rede de Ensino – Diretoria de Cooperação com Municípios & Diretoria de Orçamentos e Finanças –
 
 ## suped: SUBSECRETARIA PEDAGÓGICA
 tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeArquivo: "suped.pdf"
+titulo: Recuperação e Aprofundamento das Aprendizagens - 2o Semestre/2025
+resumo: Reforçamos a importância da preparação para as ações de Aprofundamento e Recuperação das Aprendizagens que serão promovidas pelas escolas ao final do 2o semestre letivo.
 
 
 ## efape: ESCOLA DE FORMAÇÃO E APERFEIÇOAMENTO DOS PROFISSIONAIS DA EDUCAÇÃO
 tipo: lista
-nomeArquivo: 
-titulo: 
-resumo: 
+nomeArquivo: "efape.pdf"
+titulo: 47.2025 Boletim Semanal da Subsecretaria - EFAPE
+resumo: Inscrições para o curso Escola de Gestão 1.0 - 1ª Edição - 2026 Público-alvo: Diretor, Vice-Diretor, CEC, PEC, CGPG, CGP, CGPAC e Professores Categorias A e F. Período de Inscrição se encerra em 01/12.
 `;
